92d6e2c307c7e50232a9d169131c060b1391e945
sparrow <s.l.hubbard19@gmail.com>
2024-04-24 16:49:50 -0600

    Fixed prompt issue

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
 // Import necessary functions/classes from the lib folder
 const { createLogo } = require('./lib/logo');
+const inquirer = require("inquirer")
+const fs = require('fs');
+// const prompt = require('prompt-sync')();
 
 // Run the application logic
 function runApplication() {
     // Here you can add any code to get user inputs if needed
 
-    const text = responses[3];
-    const textColor = responses[1];
-    const shape = responses[0];
-    const shapeColor = responses[2];
-
-    function askQuestion(index) {
-        if (index < questions.length) {
-            rl.question(questions[index] + " ", (answer) => {
-                responses.push(answer);
-                askQuestion(index + 1);
-            });
-        } else {
-            rl.close();
-            createLogo(text, textColor, shape, shapeColor);
-        }
-    
-    askQuestion(0);
-
-    // Call the createLogo function with the provided inputs
-    // createLogo(text, textColor, shape, shapeColor);
-    }
+    inquirer
+        .prompt([
+            {
+                name: "text",
+                message: "Enter text for the logo (up to three characters): ",
+            },
+            {
+                name: "textColor",
+                message: "Enter text color (keyword or hexadecimal): "
+            },
+            {
+                name: "shape",
+                message: "Enter shape for the logo (circle, triangle, square): "
+            },
+            {
+                name: "shapeColor",
+                message: "Enter shape color (keyword or hexadecimal): "
+            }
+        ])
+        .then(() => {
+            // Use user feedback for... whatever!!
+            fs.writeFileSync('logo.svg', createLogo(text, textColor, shape, shapeColor));
+            console.log('Generated logo.svg');
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+            } else {
+            // Something else went wrong
+            }
+        });
 }
 
 // Run the application
